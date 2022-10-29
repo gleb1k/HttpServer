afe8d741f66c2e3c6be6cd22a61c2eebab20cd6a
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-10-29 09:31:08 +0300

    table.sql

---
+++ HttpServer/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [Login]    VARCHAR (50) NOT NULL,
+    [Password] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
